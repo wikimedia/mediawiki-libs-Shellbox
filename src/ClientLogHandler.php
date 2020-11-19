78cb2b0f2dd571efc3158674fb256842c99a79ce
James D. Forrester <jforrester@wikimedia.org>
2020-11-19 09:16:57 -0800

    composer: Raise monolog requirement to 2.x, per MW 1.36
    Bug: T268207
    Change-Id: I7a73be203e0737ccf729c11806807472e158ada4

--- src/ClientLogHandler.php
+++ src/ClientLogHandler.php
@@ -8,7 +8,7 @@ class ClientLogHandler extends AbstractHandler {
 	/** @var array */
 	private $records = [];
 
-	public function handle( array $record ) {
+	public function handle( array $record ): bool {
 		$this->records[] = [
 			'level' => $record['level'],
 			'message' => $record['message'],
