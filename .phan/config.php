fbc1e22f03cf2f1c314e14d73740e30de9b4e433
Umherirrender <umherirrender_de.wp@web.de>
2022-05-03 22:41:59 +0200

    build: Use most of phan exclude_file_regex from standard
    Regex seen on mediawiki/codesniffer phan config file
    
    Change-Id: I7680c04ea7e26f6ea29f4d7f5f588fffcde50cd6

--- .phan/config.php
+++ .phan/config.php
@@ -10,8 +10,12 @@ $cfg['directory_list'] = [
 
 $cfg['exclude_analysis_directory_list'][] = 'vendor';
 
-// Default excludes phpunit/php-code-coverage, which is needed here
-$cfg['exclude_file_regex'] = '@/vendor/(phan|mediawiki|php-parallel-lint)/@';
+// Remove the exclusion of phpunit/php-code-coverage, which is needed here
+$cfg['exclude_file_regex'] = preg_replace(
+	'@\|(?:phpunit/php-code-coverage)@',
+	'',
+	$cfg['exclude_file_regex']
+);
 
 $cfg['suppress_issue_types'] = array_merge( $cfg['suppress_issue_types'], [
 	// It's a library, methods don't have to be called
